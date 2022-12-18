fa1282ad8f2e74edab69cedd5b94b88e285cebb5
ALEXANDER <msumba83@gmail.com>
2022-12-18 07:09:16 +0200

    initial commit

---
+++ js1-week1.js
@@ -0,0 +1,51 @@
+// console.log("Hello World");
+// console.log("Hallo, dunia!");
+// console.log("Ciao, mondo!");
+// console.log("Hola, mundo!");
+
+// VARIABLES
+// const greeting = "Good Morning";
+// const messageType = typeof greeting;
+// console.log(greeting);
+// console.log(greeting);
+// console.log(messageType);
+
+// STRINGS
+
+// const colors = "blue, yellow";
+// const colorType = typeof colors;
+// console.log(colorType);
+
+// String concatenation
+
+// Here is an example using the plus operator to concat strings
+// const greetingStart = "Good Morning";
+// const title = "Brother";
+// const myName = "Alexander";
+// const greeting = greetingStart + " " + title + " " + myName;
+// console.log(greeting);
+
+// Here is example using the String interpolation to concat strings
+// const greetingStart = "Good Morning";
+// const title = "Brother";
+// const myName = "Alexander";
+// const greeting = `${greetingStart} ${title} ${myName}`
+// console.log(greeting);
+
+//Numbers
+
+const numberOfTrainees = 15;
+const numberOfMentors = 8;
+
+const totalNumber = numberOfTrainees + numberOfMentors;
+
+console.log(numberOfTrainees);
+console.log(numberOfMentors);
+console.log(totalNumber)
+
+const percentOfTrainees = (numberOfTrainees * 100) / totalNumber;
+const percentOfMentors = (numberOfMentors * 100) / totalNumber;
+const numberOfTraineesRound = Math.round(percentOfTrainees)
+const numberOfMentorsRound = Math.round(percentOfMentors)
+console.log(numberOfTraineesRound);
+console.log(numberOfMentorsRound);
